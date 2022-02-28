b4128e20fcebb1f7a8ba9478468eb4a619c88272
hamendra <hamendra9675@gmail.com>
2022-02-28 19:56:39 +0530

    first commit

---
+++ resp.js
@@ -0,0 +1,15 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+burger=document.querySelector('.navbar')
+
+
+
+
+
+burger.classEventListner('click', ()=>{
+    navbar.classList.toggle('v-class');
+    nav-ist.classList.toggle('v-class');
+    right-nav.classList.toggle('v-class');
+
+
+})
